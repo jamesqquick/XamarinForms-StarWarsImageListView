e823de52b832ca93ba1afdfedbc51d373c4c27c2
James Quick <james.q.quick@outlook.com>
2016-01-04 14:18:28 -0500

    Image ListView with Star Wars Characters initial commit

---
+++ StarWars-ListViewWithImages/StarWars-ListViewWithImages/StarWars_ListViewWithImages.Droid/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace StarWars_ListViewWithImages.Droid
+{
+	[Activity (Label = "StarWars_ListViewWithImages", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new StarWars_ListViewWithImages.App ());
+		}
+	}
+}
+
---
+++ StarWars-ListViewWithImages/StarWars-ListViewWithImages/StarWars_ListViewWithImages/Home.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace StarWars_ListViewWithImages
+{
+	public class Home : ContentPage
+	{
+		Label chosenLabel;
+		public Home ()
+		{
+			Label title = new Label
+			{
+				Text = "Star Wars ListView",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.StartAndExpand,
+				FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+			};
+
+			ListView list = new ListView {
+				ItemsSource = getItems(),
+				ItemTemplate = new DataTemplate(typeof(ImageCell))
+			};
+			list.ItemTemplate.SetBinding(ImageCell.TextProperty, "Name");
+			list.ItemTemplate.SetBinding(ImageCell.ImageSourceProperty, "Source");
+
+			chosenLabel = new Label
+			{
+				Text = "You Chose",
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.StartAndExpand,
+				FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+			};
+			list.ItemTapped += List_ItemTapped;
+
+			StackLayout stack = new StackLayout
+			{
+				Children = { title, list, chosenLabel }
+			};
+			this.Content = stack;
+			this.Padding = new Thickness(10, Device.OnPlatform(20, 10, 10), 10, 10);
+		}
+
+		private void List_ItemTapped(object sender, ItemTappedEventArgs e)
+		{
+			chosenLabel.Text = ((Character)e.Item).Name;
+		}
+
+		private ObservableCollection<Character> getItems()
+		{
+			ObservableCollection<Character> characters = new ObservableCollection<Character>();
+			string[] characterNames = new string[] { "C-3PO", "Chewbacca", "Darth Vader", "Luke SkyWalker",
+			"Princess Leia","R2D2", "Yoda" };
+			string[] imageFileNames = new string[] { "C3PO.PNG", "Chewbacca.PNG", "DarthVader.PNG", "LukeSkyWalker.PNG",
+			"PrincessLeia.PNG","R2D2.PNG", "Yoda.PNG" };
+			for(int i = 0; i < characterNames.Length; i++)
+			{
+				characters.Add(new Character() { Name = characterNames[i], Source = imageFileNames[i] });
+			}
+			return characters;
+		}
+	}
+
+	public class Character
+	{
+		private string name;
+		private string source;
+		public string Name {
+			get { return name; }
+			set { name = value; }
+		}
+		public string Source {
+			get { return source; }
+			set {source = value; }
+		}
+	}
+}
